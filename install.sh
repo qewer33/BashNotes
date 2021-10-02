38554e568dd90eccff6a52b8500e2f5b449dce03
qewer33 <qewer33@github.com>
2021-10-02 13:15:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ `whoami` != 'root' ]; then
+    echo "Please run as root using sudo"
+    exit
+fi
+
 chmod u+x bashnotes
 sudo mv bashnotes /usr/bin/
 echo "BashNotes is succesfully installed"
